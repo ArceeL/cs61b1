124905095803619c5c3652d54d152734874444ae
T'er <2580849887@qq.com>
2021-05-19 16:45:29 +0800

    completed first part of lab1.

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,18 @@
+public class HelloNumbers {
+      public static void main(String[] args){
+     	int x = 0;
+	while(x<10) {
+	     System.out.print(x+" ");
+ 	     x = x+1;				
+	}
+	System.out.println();
+	x = 0;
+	int sum = 0;
+	while(x<10){
+	    sum +=x;
+	    System.out.print(sum+" ");
+	    x++;	
+	}
+	System.out.println(5 + 10);
+      }
+}
